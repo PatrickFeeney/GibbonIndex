0326c967fc3b021974f7bb1537f42318b98d6c5b
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-03-11 10:08:00 -0500

    Better Word Recognition

--- src/html_link.py
+++ src/html_link.py
@@ -8,13 +8,21 @@ topics, par_to_topic, par_texts = load_topic_data(
     "data/gibbon_paragraphs_with_topics.csv"
 )
 
-soup = BeautifulSoup("<html><head /><body /></html>", 'html.parser')
+soup = BeautifulSoup("<html><head /><body /></html>", "html.parser")
 body = soup.html.body
 for i, par_text in enumerate(par_texts):
     for word in topics[par_to_topic[i]]:
-        par_text = par_text.replace(word, '<a href="%i">%s</a>' % (i, word))
+        link_str = '<a href="%i">%s</a>' % (i, word)
+        ind = par_text.find(word)
+        while ind != -1:
+            start_ind = ind + 1
+            if (ind >= 1 and not par_text[ind - 1].isalpha()) and \
+               (ind + len(word) < len(par_text) and not par_text[ind + len(word)].isalpha()):
+                par_text = par_text[:ind] + link_str + par_text[ind + len(word):]
+                start_ind += len(link_str)
+            ind = par_text.find(word, start_ind)
     # append paragraph tag
-    par_tag = BeautifulSoup("<p>%s</p>" % (par_text,), 'html.parser').p
+    par_tag = BeautifulSoup("<p>%s</p>" % (par_text,), "html.parser").p
     body.append(par_tag)
 
 with open("output/index_debug.html", "w") as f:
