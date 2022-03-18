0928cc06310f6c6fe222fc7af77864c5c3204d05
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-03-18 11:19:22 -0400

    Revised Topic Indexing

--- src/html_link.py
+++ src/html_link.py
@@ -2,28 +2,35 @@ from bs4 import BeautifulSoup
 
 from topic_data import load_topic_data
 
-# generate HTML with links for indexing
+
 topics, par_to_topic, par_texts = load_topic_data(
     "data/gibbon_chapters_topics_lda.csv",
     "data/gibbon_paragraphs_with_topics.csv"
 )
-
+# generate HTML with links for indexing
 soup = BeautifulSoup("<html><head /><body /></html>", "html.parser")
 body = soup.html.body
 for i, par_text in enumerate(par_texts):
     for word in topics[par_to_topic[i]]:
-        link_str = '<a href="%i">%s</a>' % (i, word)
         ind = par_text.lower().find(word)
         while ind != -1:
             start_ind = ind + 1
             if (ind >= 1 and not par_text[ind - 1].isalpha()) and \
                (ind + len(word) < len(par_text) and not par_text[ind + len(word)].isalpha()):
+                link_str = '<a href="#topic%i">%s</a>' % \
+                    (par_to_topic[i], par_text[ind:ind + len(word)])
                 par_text = par_text[:ind] + link_str + par_text[ind + len(word):]
                 start_ind += len(link_str)
             ind = par_text.lower().find(word, start_ind)
     # append paragraph tag
     par_tag = BeautifulSoup("<p>%s</p>" % (par_text,), "html.parser").p
     body.append(par_tag)
+# generate index at end of page
+for i, topic in enumerate(topics):
+    topic_tag = soup.new_tag("h4", id="topic%i" % (i,))
+    topic_tag.string = "Topic %i" % (i,)
+    body.append(topic_tag)
+    body.append(soup.new_tag("br"))
 
 with open("output/index_debug.html", "w") as f:
     f.write(soup.prettify())
--- src/topic_data.py
+++ src/topic_data.py
@@ -6,7 +6,7 @@ def load_topic_data(topic_path, par_path):
     # 12 topics, top 15 words representing it
     topics = np.genfromtxt(topic_path, dtype=str, delimiter=",")[1:, 1:].T
     # load chapter text
-    par_to_topics = pd.read_csv("data/gibbon_paragraphs_with_topics.csv")
+    par_to_topics = pd.read_csv(par_path)
     par_texts = np.array(par_to_topics["StringText"], dtype=str)
     # make mapping 0-indexed
     par_to_topic = np.array(par_to_topics["LDA Topic"], dtype=int) - 1
