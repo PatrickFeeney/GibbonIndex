3b4b44024ee22a9035a9eb77f98e1de7578f2d77
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-06-03 09:59:33 -0400

    Chapter Parsing

--- src/html_link.py
+++ src/html_link.py
@@ -1,15 +1,32 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 
 from topic_data import load_topic_data
 
 
 def generate_html(topics, par_to_topic, par_texts, output_path):
+    # load data for chapter info
+    chapter_df = pd.read_csv("data/gibbon_first_with_titles.csv")
+    chapter_titles = list(chapter_df["Titles"])
+    chapter_first_pars = list(chapter_df["level_0"])
     # record paragraphs associated with each word in a topic
     topic_word_to_par = [{} for _ in range(topics.shape[0])]
     # generate HTML with links for indexing
     soup = BeautifulSoup("<html><head /><body /></html>", "html.parser")
     body = soup.html.body
+    chapter_tag = None
     for i, par_text in enumerate(par_texts):
+        # handle chapter starts by adding collapsible header
+        if chapter_first_pars.count(i) > 0:
+            chapter_ind = chapter_first_pars.index(i)
+            if chapter_tag is not None:
+                body.append(chapter_tag)
+            chapter_tag = soup.new_tag("details", id=f"chapter{chapter_ind + 1}")
+            chapter_title = soup.new_tag("b")
+            chapter_title.string = f"Chapter {chapter_ind + 1}: " + chapter_titles[chapter_ind]
+            chapter_summary = soup.new_tag("summary")
+            chapter_summary.append(chapter_title)
+            chapter_tag.append(chapter_summary)
         par_topic = par_to_topic[i]
         for word in topics[par_topic]:
             # ignore empty words
@@ -31,7 +48,9 @@ def generate_html(topics, par_to_topic, par_texts, output_path):
                 ind = par_text.lower().find(word, start_ind)
         # append paragraph tag
         par_tag = BeautifulSoup('<p id="par%i">%s</p>' % (i, par_text,), "html.parser").p
-        body.append(par_tag)
+        chapter_tag.append(par_tag)
+    # append last chapter tag
+    body.append(chapter_tag)
     # generate index at end of page
     for i, topic in enumerate(topics):
         topic_tag = soup.new_tag("details", id="topic%i" % (i,))
@@ -59,13 +78,27 @@ def generate_html(topics, par_to_topic, par_texts, output_path):
 
 if __name__ == "__main__":
     paths_list = [
-        ("data/lda_cleaned_proper_nouns.csv", "data/gibbon_paragraphs_with_topics_4_15.csv", "docs/index_debug.html"),
-        ("data/10_topics/gibbon_tm10_topics.csv", "data/10_topics/gibbon_tm10_paragraphs.csv", "docs/index_10.html"),
-        ("data/25_topics/gibbon_tm25_topics.csv", "data/25_topics/gibbon_tm25_paragraphs.csv", "docs/index_25.html"),
-        ("data/50_topics/gibbon_tm50_topics.csv", "data/50_topics/gibbon_tm50_paragraphs.csv", "docs/index_50.html"),
-        ("data/75_topics/gibbon_tm75_topics.csv", "data/75_topics/gibbon_tm75_paragraphs.csv", "docs/index_75.html"),
-        ("data/100_topics/gibbon_tm100_topics.csv", "data/100_topics/gibbon_tm100_paragraphs.csv", "docs/index_100.html"),
-        ("data/bertopic/gibbon_bertopic_topics.csv", "data/bertopic/gibbon_bertopic_paragraphs.csv", "docs/index_ber.html"),
+        ("data/lda_cleaned_proper_nouns.csv",
+         "data/gibbon_paragraphs_with_topics_4_15.csv",
+         "docs/index_debug.html"),
+        ("data/10_topics/gibbon_tm10_topics.csv",
+         "data/10_topics/gibbon_tm10_paragraphs.csv",
+         "docs/index_10.html"),
+        ("data/25_topics/gibbon_tm25_topics.csv",
+         "data/25_topics/gibbon_tm25_paragraphs.csv",
+         "docs/index_25.html"),
+        ("data/50_topics/gibbon_tm50_topics.csv",
+         "data/50_topics/gibbon_tm50_paragraphs.csv",
+         "docs/index_50.html"),
+        ("data/75_topics/gibbon_tm75_topics.csv",
+         "data/75_topics/gibbon_tm75_paragraphs.csv",
+         "docs/index_75.html"),
+        ("data/100_topics/gibbon_tm100_topics.csv",
+         "data/100_topics/gibbon_tm100_paragraphs.csv",
+         "docs/index_100.html"),
+        ("data/bertopic/gibbon_bertopic_topics.csv",
+         "data/bertopic/gibbon_bertopic_paragraphs.csv",
+         "docs/index_ber.html"),
     ]
     for topic_path, par_path, output_path in paths_list:
         topics, par_to_topic, par_texts = load_topic_data(topic_path, par_path)
